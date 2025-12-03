4715ac88cab25da2e4a6c11438b2763d76bbf7b6
Tran203 <tshetshe203@gmail.com>
2025-12-03 15:51:53 +0200

    Defined Models

---
+++ InternalResourceBookingSystem/Models/Booking.cs
@@ -0,0 +1,35 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace InternalResourceBookingSystem.Models
+{
+    public class Booking
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [ForeignKey("Resource")]
+        [DisplayName("Resource")]
+        public int? ResourceId { get; set; }
+
+        [DisplayName("Start Time")]
+        [Required(ErrorMessage = "StartTime is required")]
+        public DateTime StartTime { get; set; }
+
+        [DisplayName("End Time")]
+        [Required(ErrorMessage = "EndTime is required")]
+        public DateTime EndTime { get; set; }
+
+        [DisplayName("Booked By")]
+        [StringLength(100, ErrorMessage = "Booked by cannot exceed 100 characters")]
+        [Required(ErrorMessage = "Booked By is required")]
+        public string BookedBy { get; set; }
+
+        [Required(ErrorMessage = "Purpose is required")]
+        [StringLength(200, ErrorMessage = "Purpose cannot exceed 200 characters")]
+        public string Purpose { get; set; }
+
+        public Resource? Resource { get; set; }
+    }
+}
---
+++ InternalResourceBookingSystem/Models/Resource.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace InternalResourceBookingSystem.Models
+{
+    public class Resource
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Name is required")]
+        public string Name { get; set; }
+
+        [StringLength(500, ErrorMessage = "Description cannot exceed 500 characters")]
+        public string Description { get; set; }
+
+        [StringLength(100, ErrorMessage = "Location cannot exceed 100 characters")]
+        public string Location { get; set; }
+
+        [Range(1, int.MaxValue, ErrorMessage = "Capacity must be a positive number")]
+        public int Capacity { get; set; }
+
+        [DisplayName("Available")]
+        public bool IsAvailable { get; set; }
+
+        public ICollection<Booking>? Bookings { get; set; }
+    }
+}
