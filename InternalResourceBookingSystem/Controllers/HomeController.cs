11fee2cc852ee206b4b3509f823efda26989fdc0
Tran203 <tshetshe203@gmail.com>
2025-12-03 15:45:36 +0200

    Add project files.

---
+++ InternalResourceBookingSystem/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+using System.Diagnostics;
+using InternalResourceBookingSystem.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace InternalResourceBookingSystem.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ InternalResourceBookingSystem/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace InternalResourceBookingSystem.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
